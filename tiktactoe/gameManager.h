9a504f6501e67ee7e9ee8dac6cb2bf5f88c23133
paul <paul.sainrat.external@stormshield.eu>
2022-03-11 14:57:05 +0100

    Depart Stormshield

---
+++ tiktactoe/gameManager.h
@@ -0,0 +1,10 @@
+#ifndef __CLASS_GAME_MANAGER___
+#define __CLASS_GAME_MANAGER___
+
+class GameManager{
+private:
+public:
+
+};
+
+#endif//__CLASS_GAME_MANAGER___
--- tiktactoe/main.cpp
+++ tiktactoe/main.cpp
@@ -4,27 +4,9 @@
 
 #include "board.h"
 #include "cell.h" 
+#include "gameManager.h" 
 
-bool myFunction(CellPos a, CellPos b){ return a<b;}
 
 int main(int argc, char *argv[]){
     Board board1{4,4};
-    CellPos pos{0,0};
-    std::vector<CellPos> cellVect;
-
-    /*
-    board1.FillCross(CellPos{0,0});
-    board1.FillCross(CellPos{1,1});
-    board1.FillCross(CellPos{2,2});
-    board1.FillCross(CellPos{3,3});
-    board1.DisplayBoard();
-    */
-
-    board1.FillCircle(CellPos{0,0});
-    board1.FillCircle(CellPos{1,1});
-    board1.FillCircle(CellPos{2,2});
-    board1.FillCircle(CellPos{3,3});
-    board1.DisplayBoard();
-    std::cout << board1.CheckMatchStatus();
-
 }
